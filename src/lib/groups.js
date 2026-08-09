52b533a22539e6465445ea5e8864d2d7c22f387f
faridanangs <f4r1danangs@gmail.com>
2026-08-09 23:10:56 +0800

    ready to production

--- src/lib/groups.js
+++ src/lib/groups.js
@@ -1,10 +1,8 @@
 // Mapping nama divisi -> WhatsApp Group ID (Fonnte target format: xxxxx@g.us)
 export const GROUP_IDS = {
-  // Humkes: "120363425848765391@g.us",
-  // Perkam: "120363409780546177@g.us",
-  // Konsumsi: "120363428149058688@g.us",
-  // Acara: "120363425462901731@g.us",
-  // PDD: "120363426269393564@g.us",
-  Tes1: "120363430072381073@g.us",
-  Tes2: "120363432331143179@g.us",
+  Humkes: "120363425848765391@g.us",
+  Perkam: "120363409780546177@g.us",
+  Konsumsi: "120363428149058688@g.us",
+  Acara: "120363425462901731@g.us",
+  PDD: "120363426269393564@g.us"
 };
